de25e0137d96c244d5b54e57a919b499cca55ca0
Alvarez <e-fozv@gft.com>
2023-09-13 10:43:39 +0200

    Update producer kafka UT

--- creditcard/src/test/java/com/example/kafka/creditcard/application/BalanceEventServiceTest.java
+++ creditcard/src/test/java/com/example/kafka/creditcard/application/BalanceEventServiceTest.java
@@ -6,22 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
 
 @SpringBootTest
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"}, bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 class BalanceEventServiceTest {
 
     @Mock
     private BalanceService balanceService;
-    @Autowired
+    @Mock
     private MessagingBroker<ProducerFactory<String, Event<?>>, KafkaTemplate<String, Event<?>>> kafkaBroker;
     private BalanceEventService balanceEventService;
     private final String topic = "test-topic";
@@ -35,7 +32,7 @@ class BalanceEventServiceTest {
     }
 
     @Test
-    public void consumerTest() throws InterruptedException {
+    public void consumerOrderConfirmedTest() {
         Order order = new Order();
         order.setProductId(1);
         order.setPrice(25.0);
@@ -50,9 +47,13 @@ class BalanceEventServiceTest {
 
         Mockito.when(balanceService.getBalance(Mockito.anyInt())).thenReturn(mockBalance());
 
-        balanceEventService.consumer(orderEvent);
+        KafkaTemplate<String, Event<?>> kafkaTemplate = Mockito.mock(KafkaTemplate.class);
 
+        Mockito.when(kafkaBroker.brokerTemplate()).thenReturn(kafkaTemplate);
 
+        balanceEventService.publish(orderEvent);
+
+        Mockito.verify(kafkaTemplate, Mockito.times(1)).send(Mockito.eq(topic), Mockito.any());
     }
 
     private CreditCardBalance mockBalance() {
